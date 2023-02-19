577a78e74c3ee085a592aaddb3dd884537921790
Max Shlain <maxshlain@gmail.com>
2023-02-19 11:32:37 +0200

    add initialize ubuntu

---
+++ intialize_ubuntu.ps1
@@ -0,0 +1,31 @@
+Using module Wsl
+
+. $PSScriptRoot\lib.ps1
+
+Write-Debug "--- Initial configuration"
+
+Invoke-WslCommand -ErrorAction SilentlyContinue -DistributionName 'Ubuntu-Preview' -User 'root' -Command @'
+# configure dns
+echo "[network]" | sudo tee -a /etc/wsl.conf
+echo "generateResolvConf = false" | sudo tee -a /etc/wsl.conf
+sudo chattr -i /etc/resolv.conf 
+sudo rm -rf /etc/resolv.conf
+echo "nameserver 172.20.242.68" | sudo tee /etc/resolv.conf
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+echo "nameserver 8.8.8.4" | sudo tee -a /etc/resolv.conf
+sudo chattr +i /etc/resolv.conf
+
+# update all packages
+apt update && apt upgrade -y
+apt install -y wget
+wget -qO - https://pkg.wslutiliti.es/public.key | sudo tee /etc/apt/trusted.gpg.d/wslu.asc > /dev/null
+apt update && apt upgrade -y
+
+# install python3
+apt install -y python3
+
+# install ssh server
+apt-get install -y openssh-server
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+service ssh restart
+'@
